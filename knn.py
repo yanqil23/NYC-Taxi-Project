0af32a0a8a1d3f725cd3ccab800f77644ef9fd41
Yanqi Liu <liu2496@wisc.edu>
2024-12-05 22:56:45 -0600

    Update knn.py

--- knn.py
+++ knn.py
@@ -8,14 +8,14 @@ def process_trip_data(year_month):
     try:
         print(f"Reading data from: {year_month}")
         df = pd.read_csv(f"{year_month}_filtered.csv")
-
+        df = df[(df['trip_distance'] > 0) & (df['trip_distance'] < 60) & (df['tip_amount'] > 0)& (df['tip_amount']< 60)]
         print("Converting datetime columns...")
         df['dropoff_datetime'] = pd.to_datetime(df['dropoff_datetime'])
         df['pickup_datetime'] = pd.to_datetime(df['pickup_datetime'])
 
         print("Calculating trip duration...")
         df['trip_duration'] = (df['dropoff_datetime'] - df['pickup_datetime']).dt.total_seconds()
-
+        df = df.dropna()
         print("Preparing data for training...")
         X = df[['trip_duration', 'passenger_count', 'trip_distance']]
         y = df['tip_amount']
